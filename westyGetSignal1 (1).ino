7899370297072bc910a7a1c2736ee1eda8e80163
rfergusonwesty <47326127+rfergusonwesty@users.noreply.github.com>
2019-02-12 16:25:35 -0700

    Add files via upload

---
+++ westyGetSignal1 (1).ino
@@ -0,0 +1,25 @@
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() 
+{
+  if(Serial.available()>0)
+  {
+    char inData= Serial.read();
+    Serial.print(inData);
+    if(inData=='a')
+    {
+      digitalWrite(13,HIGH);
+      delay(500);s
+    }
+    else
+    {
+    digitalWrite(13,LOW);
+    }
+    
+  }
+
+}
